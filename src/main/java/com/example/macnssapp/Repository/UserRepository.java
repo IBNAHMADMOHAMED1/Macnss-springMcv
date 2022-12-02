b3e867056f16b14b550fef12710b6b4a65f5224f
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-02 10:57:15 +0100

    add UserRepository

--- src/main/java/com/example/macnssapp/Repository/UserRepository.java
+++ src/main/java/com/example/macnssapp/Repository/UserRepository.java
@@ -1,9 +1,7 @@
 package com.example.macnssapp.Repository;
 
 import com.example.macnssapp.Entity.User;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
+
 
 public interface UserRepository {
    User save(User user);
