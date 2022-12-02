6d1e753c549bfe91473a1a4de5f4c84495616726
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-02 10:57:15 +0100

    add UserRepositoryImpl

--- src/main/java/com/example/macnssapp/Repository/UserRepositoryImpl.java
+++ src/main/java/com/example/macnssapp/Repository/UserRepositoryImpl.java
@@ -2,16 +2,17 @@ package com.example.macnssapp.Repository;
 
 import com.example.macnssapp.Entity.User;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+
+
 
 @Component
 @Repository
 public class UserRepositoryImpl implements UserRepository {
 
-    @PersistenceContext
+
     private EntityManager em;
 
     @Override
